15ee21a8e5b656995733b9ca1967e33658cc77fb
antsmithcode <hey@antsmith.dev>
2021-03-10 18:44:49 -0500

    Psudeo

--- PleaseStandUp.js
+++ PleaseStandUp.js
@@ -1,24 +1,34 @@
 class PleaseStandUp {
-
-    /*
+  /*
     Have the function howOld(birthday) take the birthday parameter being passed and 
     return the age of a person with the date as their birthday.
     (ie. if birthday = June 22, 1996 then the output should be 24)
     */
 
-   howOld(birthday){
-        return -1;
-    }
+  howOld(birthday) {
+    /*   
 
-    /*
+            Convert the date
+
+            Define a variable to hold the data of given birthday using date method
+
+            Calculate the difference
+
+            Return the difference in years
+
+       */
+
+    return -1;
+  }
+
+  /*
     Have the function timeConvert(numOfMins) take the numOfMins parameter being passed and 
     return the number of hours, minutes, and seconds the parameter converts to 
     (ie. if num = 63.5 then the output should be 1:3:30). Separate the number of hours,
     minutes, and seconds with colons.
     */
 
-    timeConvert(numOfMins){
-        return -1;
-    }
-
+  timeConvert(numOfMins) {
+    return -1;
+  }
 }
