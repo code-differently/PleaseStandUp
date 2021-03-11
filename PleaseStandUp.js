9bc02d35653fbbb1e221b472e0b246e58f53f75e
Nkenna Ugoanusi <nkanusi@gmail.com>
2021-03-10 19:08:05 -0500

    first commit

--- PleaseStandUp.js
+++ PleaseStandUp.js
@@ -7,6 +7,14 @@ class PleaseStandUp {
     */
 
    howOld(birthday){
+       //receive the parameter and then calculate the age as follows
+    //split the input into 
+       //subract the current year from the given year
+       
+        let age = 2021 
+       //account for the month and round down if month is less than 6 and round up if month is more than 6 months
+
+       //return year and month
         return -1;
     }
 
@@ -18,7 +26,22 @@ class PleaseStandUp {
     */
 
     timeConvert(numOfMins){
-        return -1;
+        //calculate the number of hours
+        let hours = Math.floor(numOfMins/60);
+        //calculate the number of nminutes
+        let mins = Math.floor(numOfMins%60);
+        // calculate the number of seconds
+        //first calcate the modular to 2 decimal places
+        let seconds = (numOfMins % 60).toFixed(2) ;
+        // then split result by dot
+        let newArray = seconds.split('.');
+        //find the index after the dot and multiply that by .6
+       let second  = newArray[1]* .6;
+            
+    //return hour minute and seconds
+        return `${hours}:${mins}:${second}`;
     }
 
 }
+
+module.exports = PleaseStandUp;
