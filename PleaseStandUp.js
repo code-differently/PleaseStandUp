fb33ec45cb2242d453ab36455d54a13bd3cb95a6
Hakim Mackey <hakimmackey@Hakims-Air.fios-router.home>
2021-03-10 19:09:56 -0500

    added PleaseStandUp

--- PleaseStandUp.js
+++ PleaseStandUp.js
@@ -10,6 +10,19 @@ class PleaseStandUp {
         return -1;
     }
 
+    howOld(birthday)
+    // calculate month
+    let month =       // 12 months in a year
+    // calculate day
+    let day =           // 365 days in a year
+    // calculate year
+    let year =      // 4 numerical values/positions
+    // results / output of month, day, year / put into an array
+    let results = $["month", "day", "year"] 
+    // output results
+    return results
+
+
     /*
     Have the function timeConvert(numOfMins) take the numOfMins parameter being passed and 
     return the number of hours, minutes, and seconds the parameter converts to 
@@ -22,3 +35,18 @@ class PleaseStandUp {
     }
 
 }
+
+timeConvert(numOfMins)
+// take the num value and then divide it by 60 min to give the hours in the num value
+
+// if there is a remainder from the hour value, then the remainder of num will populate the minute value
+
+// if there is a remainder from the minute value, then remainder will populate the seconds value
+
+// put all the values in an array and add ":" to separate the hour, minute and second values.
+
+// results / output of the array
+
+
+
+module.exports = PleaseStandUp;
