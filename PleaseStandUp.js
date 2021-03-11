fa1c44dd4283e30bb644869cae674e3cb881f8a0
seagreenmedia <erinjenkins57@gmail.com>
2021-03-10 19:56:48 -0500

    timeConvert complete

--- PleaseStandUp.js
+++ PleaseStandUp.js
@@ -8,7 +8,7 @@ class PleaseStandUp {
 
     howOld(birthday) {
         // store the birthday parameter in a variable
-        // convert string to integer
+        // convert string to number
         // if today's month and day is after given birth month and day
         // subtract birth year from today's year
         // return integer
@@ -16,6 +16,10 @@ class PleaseStandUp {
         // subtract birth year from today's year
         // subtract 1
         // return integer  
+
+        //or 
+
+        // convert to milliseconds & subtract
         return -1;
     }
 
@@ -35,8 +39,9 @@ class PleaseStandUp {
         // return results 
 
         let hours = Math.floor(numOfMins / 60);
-        let minutes = Math.floor(numOfMins % 60);
-        let seconds = Math.floor(minutes / 60);
+        let minutes = Math.trunc(numOfMins) % 60;
+        let seconds = (numOfMins % 60 % 1);
+        seconds = Math.floor(seconds * 60);
         let results = `${hours}:${minutes}:${seconds}`;
         return results;
         // also return `${parseInt{num / 60}, 10)}:${num % 60}`
