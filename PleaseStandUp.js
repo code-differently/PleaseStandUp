b30fca477bec7f838cb3b70a987b752d42e95b31
SpringerJordan <SpringerJordan>
2021-03-10 19:03:01 -0500

    first commit

--- PleaseStandUp.js
+++ PleaseStandUp.js
@@ -7,6 +7,14 @@ class PleaseStandUp {
     */
 
    howOld(birthday){
+    // subtract the difference between the year inseterted and todays year get A
+    // if the month inserted is > then todays month plus 1
+    // if the month inserted is = to todays month then 
+    // if the day inserted is greater the todays day + 1 if not add 0.
+    // add A + B = year old
+    // return year old
+
+
         return -1;
     }
 
@@ -18,7 +26,33 @@ class PleaseStandUp {
     */
 
     timeConvert(numOfMins){
-        return -1;
+        // divide number by 60 get A
+        // divide number by 60, remainder round down, get B
+        // divide number by 60, remainder minus whole number? remainder multiplied by .6, get C
+        // A + : + B + : + C
+
+        let hours = Math.floor(numOfMins/60);
+        // console.log(hours);
+
+        let minutes = Math.floor(numOfMins%60);
+        // console.log(B);
+        
+        
+        
+        
+        
+        
+        let C = numOfMins % 1;
+        // console.log(C);
+        let D = C * .6;
+        // console.log(D);
+        let seconds = Math.floor(D * 100);
+
+        return hours + ":" + minutes + ":" + seconds;
+        
+    
+
     }
 
 }
+module.exports = PleaseStandUp;
