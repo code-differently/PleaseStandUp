310896ef8a855e650bde6a2b246dbd832b3d9184
cv-net <khalil.mktg@gmail.com>
2021-03-11 20:39:39 -0500

    Done

--- PleaseStandUp.js
+++ PleaseStandUp.js
@@ -7,7 +7,17 @@ class PleaseStandUp {
     */
 
    howOld(birthday){
-        return -1;
+        var today = new Date();
+        console.log(today)
+        var birthDate = new Date(birthday);
+        console.log(birthday)
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        //If the birth month hasn't passed yet, then subtract a year from the age
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        } 
+        return age;
     }
 
     /*
@@ -18,7 +28,16 @@ class PleaseStandUp {
     */
 
     timeConvert(numOfMins){
-        return -1;
+        //divide time by 60, and round down to get hours
+        let hours = Math.floor(numOfMins / 60);
+        //get remainder of dividing time by 60 to get minutes
+        let minutes = Math.floor(numOfMins % 60);
+        //get remainder of dividing time by its rounded integer to get seconds
+        let floor = Math.floor(numOfMins);
+        let secondsDecimal = (numOfMins % floor).toFixed(2);
+        let seconds = 60 * secondsDecimal;
+        //concatenate variables        
+        return `${hours}:${minutes}:${seconds}`;
     }
 
 }
