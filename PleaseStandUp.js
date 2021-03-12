7fce91dea0a084864e9fdb7a1822edd8b07a8b0f
Juan Avalos <juanavalos@Juans-MacBook-Air.local>
2021-03-11 19:07:18 -0500

    first commit

--- PleaseStandUp.js
+++ PleaseStandUp.js
@@ -7,6 +7,12 @@ class PleaseStandUp {
     */
 
    howOld(birthday){
+       //subtract today's date with date of birthday to get age in munber of years
+            //define today's date
+            //convert today's date in to a number that can be subtracted from (2021/3/11)
+            //convert given birthday in to the same format as today's date (1996/6/22)
+            //subtract today's date by birthday
+            //return total
         return -1;
     }
 
@@ -18,7 +24,14 @@ class PleaseStandUp {
     */
 
     timeConvert(numOfMins){
-        return -1;
+        //calculate number of hours by divinding input by 60 then rounding down
+        var hours = Math.floor(numOfMins/60);
+        //calculate number of mins subtracting input by hours converted into minutes then rounding down
+        var minutes = Math.floor(numOfMins - (hours*60));
+        //calculate number of secs by subtracting input by hours converted into minutes plus minutes then multiplying by 60 then rounding down
+        var seconds = Math.floor((numOfMins - ( (hours*60) + minutes ) ) * 60);
+        //return hours, minutes, and secs into a combined output separated by semicolons
+        return hours + ":" + minutes + ":" + seconds;
     }
 
 }
