b85cda3eebfad87181e7be92421ccedf3e60ebb9
Cinitae <74632407+Cinitae@users.noreply.github.com>
2021-03-11 19:12:26 -0500

    Barely made it, but something is turned in

--- PleaseStandUp.js
+++ PleaseStandUp.js
@@ -7,6 +7,9 @@ class PleaseStandUp {
     */
 
    howOld(birthday){
+       //Get full birthday ex: mm dd, yyyy
+       //Get todays full date ex: mm dd, yyyy
+       //Subtract date abd birthday year
         return -1;
     }
 
@@ -16,7 +19,9 @@ class PleaseStandUp {
     (ie. if num = 63.5 then the output should be 1:3:30). Separate the number of hours,
     minutes, and seconds with colons.
     */
-
+//Take the number being passed and divide the whole number by 60 for the hour(s)
+//Take the remainder for the minute
+//Take the number after the decimal for seconds
     timeConvert(numOfMins){
         return -1;
     }
